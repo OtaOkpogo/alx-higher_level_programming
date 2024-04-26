3370e767aa1214d467a41ba3b24faecf4b74009e
OtaOkpogo <otaokpogo@yahoo.com>
2024-04-26 07:11:53 -0700

    Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8)

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """
-function that Sends a request to the URL and displays the body of the response
-(decoded in utf-8).
+A Python script that takes in a URL, sends a request and displays
+the value of the X-Request-Id variable found in the header
+(handling HTTP errors)
 """
+from urllib import request, error
+import sys
 
 
-if __name__ == '__main__':
-    import sys
-    from urllib import request, error
-
-    argv = sys.argv
-    url = argv[1]
+if __name__ == "__main__":
     try:
-        with request.urlopen(url) as response:
-            print(response.read().decode('utf-8'))
+        with request.urlopen(sys.argv[1]) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
     except error.HTTPError as err:
-        print("Error code: {}".format(err.status))
+        print('Error code: {}'.format(err.code))
